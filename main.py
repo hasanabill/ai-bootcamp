f6d92b7f22ecfba36434d29f5e20f7fc248e9f93
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-08-24 15:16:11 +0600

    simple linear regression model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 sqft_train = np.random.randint(1000, 3000, 1000)
@@ -12,3 +12,7 @@ price_train = 50 * sqft_train + np.random.normal(0, 5000, 1000)
 # print(price_train)
 for s, p in zip(sqft_train[0:1000], price_train[0:1000]):
     print(s, p)
+
+sqft_train_mean = np.mean(sqft_train)
+sqft_train_std = np.std(sqft_train)
+sqft_train_normalized = (sqft_train - sqft_train_mean) / sqft_train_std
